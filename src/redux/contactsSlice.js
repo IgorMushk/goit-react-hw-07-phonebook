8d7d58ba233d088e77a6121755f3f5043bc92433
IgorMushk <igor.mushketik@gmail.com>
2023-10-12 17:50:09 +0300

    upd contactsSlice.js  operations.js (createAsyncThunk )

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,34 +1,61 @@
 import { createSlice } from '@reduxjs/toolkit';
-
-const tasksInitialState = {
-  items: [],
-  isLoading: false,
-  error: null,
-};
+import { fetchContacts } from './operations';
 
 const contactsSlice = createSlice({
-  name: 'contacts',
-  initialState: tasksInitialState,
-  reducers: {
-    fetchingInProgress(state) {
-      state.isLoading = true;
+    name: "contacts",
+    initialState: {
+      items: [],
+      isLoading: false,
+      error: null,
     },
-    fetchingSuccess(state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
+    extraReducers: {
+      [fetchContacts.pending](state) {
+        state.isLoading = true;
+      },
+      [fetchContacts.fulfilled](state, action) {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      },
+      [fetchContacts.rejected](state, action) {
+        state.isLoading = false;
+        state.error = action.payload;
+      },
     },
-    fetchingError(state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-  },
-});
-
-export const {
-    fetchingInProgress,
-    fetchingSuccess,
-    fetchingError,
-  } = contactsSlice.actions;
+  });
+  
   export const contactsReducer = contactsSlice.reducer;
+
+
+// const tasksInitialState = {
+//   items: [],
+//   isLoading: false,
+//   error: null,
+// };
+
+// const contactsSlice = createSlice({
+//   name: 'contacts',
+//   initialState: tasksInitialState,
+//   reducers: {
+//     fetchingInProgress(state) {
+//       state.isLoading = true;
+//     },
+//     fetchingSuccess(state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.items = action.payload;
+//     },
+//     fetchingError(state, action) {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     },
+//   },
+// });
+
+// export const {
+//     fetchingInProgress,
+//     fetchingSuccess,
+//     fetchingError,
+//   } = contactsSlice.actions;
+//   export const contactsReducer = contactsSlice.reducer;
   
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,14 +1,27 @@
 import axios from "axios";
-import { fetchingError, fetchingInProgress, fetchingSuccess } from "./contactsSlice";
+//import { fetchingError, fetchingInProgress, fetchingSuccess } from "./contactsSlice";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://6527d88c931d71583df17a8b.mockapi.io";
 
-export const fetchContacts = () => async dispatch => {
-  try {
-    dispatch(fetchingInProgress());
-    const response = await axios.get("/contacts");
-    dispatch(fetchingSuccess(response.data));
-  } catch (error) {
-    dispatch(fetchingError(error.message));
-  }
-};
+export const fetchContacts = createAsyncThunk(
+    "contacts/fetchAll",
+    async (_, thunkAPI) => {
+      try {
+        const response = await axios.get("/contacts");
+        return response.data;
+      } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+      }
+    }
+  );
+
+// export const fetchContacts = () => async dispatch => {
+//   try {
+//     dispatch(fetchingInProgress());
+//     const response = await axios.get("/contacts");
+//     dispatch(fetchingSuccess(response.data));
+//   } catch (error) {
+//     dispatch(fetchingError(error.message));
+//   }
+// };
